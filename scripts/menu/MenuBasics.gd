f48472a18ed6b7fdb5c7a08f3f9b717a68e6842d
Caio M.G.A <caiomga@gmail.com>
2024-02-03 22:47:54 -0300

    create title screen flow; create boss rush and single stage scenes; add difficulty selection; add game mode selection; add stage selection

--- scripts/menu/MenuBasics.gd
+++ scripts/menu/MenuBasics.gd
@@ -1,7 +1,19 @@
 extends Control
 
+@export var game_mode_label:RichTextLabel
+@export var difficulty_label:RichTextLabel
+
+
 func _on_rich_text_label_meta_clicked(meta):
 	OS.shell_open(str(meta))
 
 func _on_btn_back_button_down():
 	visible = false
+
+func setup(game_mode:String, difficulty:String, stage_select = -1):
+	if game_mode=="BOSS_RUSH":
+		game_mode_label.text = "BOSS RUSH"
+	else :
+		game_mode_label.text = "Stage %d" % [stage_select]
+	
+	difficulty_label.text = difficulty
--- scripts/rooms/TitleScreen.gd
+++ scripts/rooms/TitleScreen.gd
@@ -4,9 +4,13 @@ extends Control
 @export var menu_credits:Node
 @export var menu_about:Node
 
+@export var menu_pregame:Node
+
 var pathFollow2D:PathFollow2D
 var floatingSpeed = 15
 var selected_game_mode = ""
+var selected_difficulty = ""
+var selected_stage = 1
 
 func _ready():
 	pathFollow2D = $Path2D/PathFollow2D
@@ -15,11 +19,9 @@ func _ready():
 func _process(delta):
 	pathFollow2D.progress += delta * floatingSpeed
 
-
 func _on_btn_boss_rush_button_down():
 	selected_game_mode = "BOSS_RUSH"
 	menu_difficulty_select.visible = true
-
 
 func _on_btn_stage_select_button_down():
 	selected_game_mode = "STAGE_SELECT"
@@ -28,6 +30,31 @@ func _on_btn_stage_select_button_down():
 func _on_btn_credits_button_down():
 	menu_credits.visible = true
 
-
 func _on_btn_about_button_down():
 	menu_about.visible = true
+
+func _on_btn_stage_button_down(stage_number):
+	print(stage_number)
+	selected_stage = stage_number
+	menu_difficulty_select.visible = true
+
+func _on_btn_normal_mode_button_down():
+	selected_difficulty = "NORMAL"
+	show_pre_game_confirmation()
+
+func _on_btn_hard_mode_button_down():
+	selected_difficulty = "HARD"
+	show_pre_game_confirmation()
+
+func show_pre_game_confirmation():
+	menu_pregame.setup(selected_game_mode, selected_difficulty, selected_stage)
+	menu_pregame.visible = true
+
+
+func _on_pregame_start_button_down():
+	if selected_game_mode == "BOSS_RUSH":
+		var scene = "res://scenes/rooms/boss_rush_room.tscn"
+		get_tree().change_scene_to_file(scene)
+	else:
+		var scene = "res://scenes/rooms/single_stage.tscn"
+		get_tree().change_scene_to_file(scene)
