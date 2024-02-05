5e3c676b4299cafcb6dc5d3e1e6b34bae7209db0
Caio M.G.A <caiomga@gmail.com>
2024-02-05 14:32:41 -0300

    add gameplay music

---
+++ scripts/controllers/SoundGuy.gd
@@ -0,0 +1,24 @@
+extends Node
+
+var stage_loop = []
+var loop_track_index = -1
+var audio_stream_player:AudioStreamPlayer
+
+func _ready():
+	audio_stream_player = $AudioStreamPlayer
+	audio_stream_player.stream = load("res://audio/gameplay_intro.ogg")
+	stage_loop.append(preload("res://audio/gameplay_a.ogg"))
+	stage_loop.append(preload("res://audio/gameplay_b.ogg"))
+	
+	audio_stream_player.play()
+	
+func _on_audio_stream_finish():
+	play_loop()
+
+func play_loop():
+	loop_track_index += 1
+	if loop_track_index >= stage_loop.size():
+		loop_track_index = 0
+	
+	audio_stream_player.stream = stage_loop[loop_track_index]
+	audio_stream_player.play()
